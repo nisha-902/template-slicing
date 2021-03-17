887f39225171f5877525d5cc41eb58aaceb59820
Nisha <nk1655550@gmail.com>
2021-03-17 12:26:07 -0400

    bootstrap template slicing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
+import { ERROR_COMPONENT_TYPE } from '@angular/compiler';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+import { FormsComponent } from './forms/forms.component';
+import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './login/login.component';
+import { TableComponent } from './table/table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ {path:'',redirectTo:'login',pathMatch:'full'},
+ {path:'login',component:LoginComponent},
+ {
+   path:'layout', component:LayoutComponent,
+   children:[
+     
+      {path:'dashboard',component:DashboardComponent},
+      {path:'login',component:LoginComponent},
+      {path:'forms', component:FormsComponent},
+      {path:'tab', component:TableComponent}
+     
+   ]
+ },
+ {path:'**',component:ErrorComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { ButtonComponent } from './button/button.component';
+import { TableComponent } from './table/table.component';
+import { FormsComponent } from './forms/forms.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TopbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    ButtonComponent,
+    TableComponent,
+    FormsComponent,
+    LoginComponent,
+    LayoutComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
